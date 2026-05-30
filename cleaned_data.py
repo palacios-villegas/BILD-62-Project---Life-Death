8a2e979445f97cee7f72c2440a7add8d076632f5
palacios-villegas <d2palacios@ucsd.edu>
2026-05-29 23:58:51 -0700

    Death data into array so far

--- cleaned_data.py
+++ cleaned_data.py
@@ -1,1 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
+file_contents = open("Deaths_1x1.txt").read()
+file_contents = file_contents.replace('+', '')
+
+with open("Deaths_1x1.txt", "w", encoding="utf-8") as file:
+    file.write(file_contents)
+
+raw_death_data = np.loadtxt(fname='Deaths_1x1.txt')
+death_data = np.array(raw_death_data)
+print(death_data)
+print(death_data.shape)
