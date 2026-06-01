6cd7a3b0bd11dbeff2bec12f4a1ee6418b1f3a92
palacios-villegas <d2palacios@ucsd.edu>
2026-05-31 19:19:24 -0700

    05/31/26 7:19pm update

--- cleaned_birth_data.py
+++ cleaned_birth_data.py
@@ -2,14 +2,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-file_birth_contents = open('DNKbirthsTR.txt').read()
+file_birth_contents = open('DNKbirthsTR_01.txt').read()
 file_birth_contents = file_birth_contents.replace('+', '')
 file_birth_contents = file_birth_contents.replace('-', '')
-file_birth_contents = file_birth_contents.replace(' 0.00 ', '.')
+file_birth_contents = file_birth_contents.replace('  . ', '0000 ')
+print(file_birth_contents)
 
-with open("DNKbirthsTR.txt", "w", encoding="utf-8") as file:
+with open("DNKbirthsTR.txt_01", "w", encoding="utf-8") as file:
     file.write(file_birth_contents)
 
-birth_data = np.array(np.loadtxt(fname='DNKbirthsTR.txt'))
+birth_data = np.array(np.loadtxt(fname='DNKbirthsTR_01.txt'))
 total_births = birth_data[:, 3]
 print(birth_data)
