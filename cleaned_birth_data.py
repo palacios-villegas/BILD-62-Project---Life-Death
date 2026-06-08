bc11a9b2a8597678f5abd2efe854d3fbf91c2591
palacios-villegas <d2palacios@ucsd.edu>
2026-06-07 17:47:55 -0700

    06/07/26 5:47pm update

--- cleaned_birth_data.py
+++ cleaned_birth_data.py
@@ -25,19 +25,22 @@ border = np.empty([0])
 array_data = np.empty([0, 4])
 
 for index, births in enumerate(birth_column):
-    if birth_data[index, 1] == 12 or birth_data[index, 1] == 55:
-        births = np.array(births)
+    if birth_data[index, 1] == 12 or birth_data[index, 1] == 55:  # takes care of the border cases,
+        births = np.array(births)  # ie when mothers are 12 or 55
         births = births[np.newaxis]
         border = np.append(border, births, axis=0)
         added_birth = np.append(added_birth, border, axis=0)
         border = np.empty([0])
 
+    # sums together the births of women giving birth at the same age just born different years
     elif birth_data[index, 1] == birth_data[index+1, 1]:
+        # 3, because 4th column has
         sum_births = np.array(birth_data[index, 3] + birth_data[index+1, 3])
-        sum_births = sum_births[np.newaxis]
+        sum_births = sum_births[np.newaxis]  # num of births
         added_birth = np.append(added_birth, sum_births, axis=0)
 
 # gives array with birth year of child on axis 0, age of mother on axis 1
 
 
+# 44, because 44 ages in the data set
 age_birth_array = make_array(added_birth, 44)
--- death_visualizations.py
+++ death_visualizations.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 from cleaned_pop_death_data import total_death_array, male_death_array, female_death_array
 
 age = np.arange(0, 111)
@@ -8,13 +9,10 @@ transposed_tot_death = total_death_array.T
 transposed_m_death = male_death_array.T
 transposed_f_death = female_death_array.T
 
+
 tot_death_percent = transposed_tot_death / \
     transposed_tot_death.sum(axis=0) * 100
-m_death_percent = transposed_m_death / \
-    transposed_m_death.sum(axis=0) * 100
-f_death_percent = transposed_f_death / \
-    transposed_f_death.sum(axis=0) * 100
-# above gives %female death out of all female deaths
+# above gives %deaths
 # below gives %female death out of all female AND male deaths
 mtot_death_percent = transposed_m_death / \
     transposed_tot_death.sum(axis=0) * 100
@@ -22,7 +20,7 @@ ftot_death_percent = transposed_f_death / \
     transposed_tot_death.sum(axis=0) * 100
 
 
-def plot_heatmap(data, years=year, ages=age, y_axis=np.arange(5, 105, 10), title=str):
+def plot_heatmap(data, years=year, ages=age, y_axis=np.arange(5, 105, 10), title=str, color='viridis', cmap_title='Deaths (%)'):
     """
     Plots a heatmap showing the distribution of births by mother's age and child birth year.
 
@@ -48,10 +46,10 @@ def plot_heatmap(data, years=year, ages=age, y_axis=np.arange(5, 105, 10), title
         data,
         aspect='auto',
         origin='lower',
-        extent=[years[0], years[-1], ages[0], ages[-1]]
+        extent=[years[0], years[-1], ages[0], ages[-1]],
+        cmap=f'{color}'
     )
-
-    plt.colorbar(label='Deaths (%)')
+    plt.colorbar(label=cmap_title)
     plt.xlabel('Year of Death')
     plt.ylabel('Age at Death')
     plt.xticks(np.arange(1835, 2025, 20))
@@ -62,33 +60,43 @@ def plot_heatmap(data, years=year, ages=age, y_axis=np.arange(5, 105, 10), title
 
 
 def censor_data(uncensored_data):
-    return uncensored_data[2:, :], uncensored_data[:3, :]
+    return uncensored_data[3:, :], uncensored_data[:3, :]
 
 
 ci_tot_death_percent, ca_tot_death_percent = censor_data(
     tot_death_percent)  # ci -> censored infants, ca -> censored adults
-
-
 ci_mtot_death_percent, ca_mtot_death_percent = censor_data(
     mtot_death_percent)
 ci_ftot_death_percent, ca_ftot_death_percent = censor_data(ftot_death_percent)
 
-
-ci_sex_difference_death_percent = ci_mtot_death_percent - ci_ftot_death_percent
-ca_sex_difference_death_percent = ca_mtot_death_percent - ca_ftot_death_percent
-# difference between male and female deaths. >0% means more male deaths, <0% means more female deaths
+ci_sex_difference_death_percent = ci_ftot_death_percent - ci_mtot_death_percent
+# difference between female and male deaths. >0% means more female deaths, <0% means more male deaths
 
 
-plot_heatmap(tot_death_percent, title='Total Population')
-plot_heatmap(ca_tot_death_percent, y_axis=np.arange(
+plot_heatmap(ci_tot_death_percent, y_axis=np.arange(  # FIGURE 6 ################################################################
+    5, 105, 10), title='Adults')
+
+plot_heatmap(ca_tot_death_percent, y_axis=np.arange(  # FIGURE 8 #############################################################
     0, 3), ages=np.arange(-1, 3, 1), title='Infants')
-plot_heatmap(ci_tot_death_percent, title='Adults')
 
 
-# better visualization through sex difference than sex alone
-plot_heatmap(ci_sex_difference_death_percent, title='Adult Sex Difference')
-plot_heatmap(ca_sex_difference_death_percent, y_axis=np.arange(
-    0, 3), ages=np.arange(-1, 3, 1), title='Infant Sex Difference')
+# FIGURE 7 ###############################################################################################################
+plt.figure(figsize=(10, 5))
+plt.imshow(
+    ci_sex_difference_death_percent,  # better visualization through sex difference
+    aspect='auto',
+    origin='lower',
+    extent=[year[0], year[-1], age[0], age[-1]],
+    cmap='bwr', norm=colors.CenteredNorm()
+)
+plt.colorbar(label='Female (%)deaths - Male (%)deaths')
+plt.xlabel('Year of Death')
+plt.ylabel('Age at Death')
+plt.xticks(np.arange(1835, 2025, 20))
+plt.yticks(np.arange(5, 105, 10))
+plt.title(f"Age at Death vs Year in Adult Sex %Death Difference")
+plt.tight_layout()
+plt.show()
 
 
 def calc_average(counts, ages):
@@ -104,13 +112,13 @@ def calc_average(counts, ages):
     return average_age
 
 
-# plot line graph
+# calculate averages for everyone, and infant-censored
 average_tot_death = calc_average(total_death_array, age)
 ci_average_tot_death = calc_average(
     total_death_array[:, 3:], np.arange(3, 111))
-ca_average_tot_death = calc_average(
-    total_death_array[:, 3:], np.arange(0, 3))
 
+
+# plot line graph ############## FIGURE 9 ####################################################################################
 plt.figure(figsize=(10, 5))
 plt.plot(year, average_tot_death, label='Average Deaths')
 plt.plot(year, ci_average_tot_death, label='Average Deaths Without Infants')
@@ -118,6 +126,27 @@ plt.xticks(np.arange(1835, 2025, 20))
 plt.title("Average Age at Death vs Year")
 plt.xlabel('Year of Death')
 plt.ylabel('Average Age at Death')
+
+# plot special points !!
+x = [1918, 1945, 1865, 1849]
+y = [60, 67, 38, 50]
+txt = ['World War I', 'World War II',
+       'Second Schleswig War', 'First Schleswig War']
+plt.axvspan(1914, 1918, color="yellow", alpha=0.3)
+plt.axvspan(1939, 1945, color="yellow", alpha=0.3)
+plt.axvspan(1848, 1852, color="yellow",
+            alpha=0.3)
+plt.axvspan(1864, 1865, color="yellow",
+            alpha=0.3)
+
+for i, year in enumerate(x):
+    plt.annotate(
+        text=(f'{txt[i]}'),
+        xy=(x[i], y[i]),
+        xytext=(0, 10),
+        textcoords="offset points",
+        ha='center',
+        fontsize=7)
 plt.legend()
 plt.tight_layout()
 plt.show()
